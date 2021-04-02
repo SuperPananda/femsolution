e2400a9e82ad505c65e0512830e5d667adc87cf5
Admin <superpananda@gmail.com>
2021-04-02 16:01:29 +0700

    fix

--- femsolution/AnalyticalSolution.cs
+++ femsolution/AnalyticalSolution.cs
@@ -13,26 +13,15 @@ namespace femsolution
         /// <summary>
         /// Получить аналитическре решение
         /// </summary>
-        /// <param name="area"></param>
+        /// <param name="nodes"></param>
         public void GetAnalyticalSolution(Nodes[] nodes)
         {
-            var x = new List<string>();
-            var y = new List<string>();
-            var z = new List<string>();
+            var outFile = new List<string>();
             for (int i=0; i < nodes.Length; i++)
             {
-                x.Add(nodes[i].Point.X.ToString() + " " + nodes[i].Point.Y.ToString() + " " + (Math.Pow(nodes[i].Point.X, 2) + Math.Pow(nodes[i].Point.Y, 2)).ToString());
-                y.Add(nodes[i].Point.Y.ToString());
-                z.Add((Math.Pow(nodes[i].Point.X, 2) + Math.Pow(nodes[i].Point.Y, 2)).ToString());
-                Console.WriteLine("X: {0} Y: {1} Ez: {2}", nodes[i].Point.X, nodes[i].Point.Y, Math.Pow(nodes[i].Point.X, 2) + Math.Pow(nodes[i].Point.Y, 2));
+                outFile.Add(nodes[i].Point.X.ToString() + " " + nodes[i].Point.Y.ToString() + " " + (Math.Pow(nodes[i].Point.X, 2) + Math.Pow(nodes[i].Point.Y, 2)).ToString());
             }
-            File.WriteAllLines("out.txt", x);
-           // File.WriteAllLines("outY.txt", y);
-            //File.WriteAllLines("outZ.txt", z);
-
-
-
-            Console.WriteLine();
+            File.WriteAllLines("out.txt", outFile);
         }
     }
 }
